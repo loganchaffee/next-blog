a22732eb90e6d0b8491d1fd97c634b6e4dc0b984
Logan Chaffee <loganschaffee@gmail.com>
2022-10-15 14:51:16 -0700

    fix build issue

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,6 @@ export default function Home({ allPostsData }) {
             </Head>
             <section className={utilStyles.headingMd}>
                 <p>I am a web dev. This is my blog</p>
-                <p>
-                (This is a sample website - you'll be building a site like this on 
-                <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
             </section>
             <section>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
